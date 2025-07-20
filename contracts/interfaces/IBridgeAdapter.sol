9ddce2ffe51f4f745217d055038f071463e9a159
Diego <136717693+ciphermalware@users.noreply.github.com>
2025-07-20 09:38:19 +0200

    Update IBridgeAdapter.sol

--- contracts/interfaces/IBridgeAdapter.sol
+++ contracts/interfaces/IBridgeAdapter.sol
@@ -1,1 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
+interface IBridgeAdapter {
+    function bridgeTokens(
+        string calldata bridgeName,
+        address token,
+        uint256 amount,
+        uint256 dstChainId,
+        bytes calldata recipient
+    ) external;
+}
